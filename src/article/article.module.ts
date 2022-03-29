aa9f8200acf4709b31493ab1ba544d62532b3bbf
yuraTsBackEnd <yurii@surelockkey.com>
2022-03-30 00:46:31 +0300

    add article entity

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleController } from './article.controller';
+import { ArticleEntity } from './article.entity';
 import { ArticleService } from './article.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([ArticleEntity])],
   controllers: [ArticleController],
   providers: [ArticleService],
 })
